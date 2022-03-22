e80fc4c136eeee0f373a8940ea0d00efbeaf60d0
EJEvan1 <43522005+EJEvan1@users.noreply.github.com>
2022-03-22 13:24:15 +0000

    Initial commit

---
+++ images/ImageFileFilter.java
@@ -0,0 +1,23 @@
+/* Decompiler 12ms, total 199ms, lines 23 */
+package images;
+
+import java.io.File;
+import javax.swing.filechooser.FileFilter;
+
+public class ImageFileFilter extends FileFilter {
+   private String extension;
+   private String description;
+
+   public ImageFileFilter(String var1, String var2) {
+      this.extension = var1;
+      this.description = var2;
+   }
+
+   public boolean accept(File var1) {
+      return var1.getName().toLowerCase().endsWith(this.extension) || var1.isDirectory();
+   }
+
+   public String getDescription() {
+      return this.description;
+   }
+}
---
+++ readME.txt
@@ -0,0 +1,2 @@
+This is the decomplied version of the images.jar 
+file we used in class.
